1757edab3c0195c8954564683d8fba9fbc6bd767
InhyeKang <kanginhae0826@gmail.com>
2022-05-19 22:45:20 +0900

    [ADD] COMPLETE APPLY UI

---
+++ DMSChallenge/DMSChallenge/MenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MenuViewController.swift
+//  DMSChallenge
+//
+//  Created by 강인혜 on 2022/05/18.
+//
+
+import Foundation
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
