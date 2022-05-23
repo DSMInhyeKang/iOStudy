6970e1f08e063e91de50b9bebcdff899390bb910
InhyeKang <kanginhae0826@gmail.com>
2022-05-23 23:04:24 +0900

    [ADD] BRING SPINDICATOR

--- DMSChallenge/DMSChallenge/MenuViewController.swift
+++ DMSChallenge/DMSChallenge/MenuViewController.swift
@@ -5,13 +5,21 @@
 //  Created by 강인혜 on 2022/05/18.
 //
 
-import Foundation
 import UIKit
+import SPIndicator
 
 class MenuViewController: UIViewController {
+    @IBOutlet weak var breakfastView: UIView!
+    @IBOutlet weak var lunchView: UIView!
+    @IBOutlet weak var dinnerView: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        SPIndicator.present(title: "Error", message: "Try Again", preset: .error)
+        breakfastView.layer.cornerRadius = 20
+        lunchView.layer.cornerRadius = 20
+        dinnerView.layer.cornerRadius = 20
 
     }
 
